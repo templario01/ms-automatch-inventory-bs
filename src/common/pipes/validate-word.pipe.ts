133a48cc001a9855dde10e905f796f0db36e54cb
Victor Manuel Benavente Yepez <88349543+victorbenavente@users.noreply.github.com>
2024-01-29 23:42:14 -0500

    feat: modify kubernetes infra

--- src/common/pipes/validate-word.pipe.ts
+++ src/common/pipes/validate-word.pipe.ts
@@ -9,7 +9,6 @@ import {
 export class ValidateWordPipe implements PipeTransform {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   transform(value: any, metadata: ArgumentMetadata) {
-    console.log(value);
     if (!value) {
       throw new BadRequestException(`Query param is missing "?word="`);
     }
